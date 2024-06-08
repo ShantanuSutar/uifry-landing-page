db9ba41b4dcd76f893a6532d59c28c1cc99803fc
Shantanu Sutar <shantanusutar17@gmail.com>
2024-06-09 03:05:40 +0530

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import Logo from "../assets/Logo.png";
 import { LiaToggleOffSolid } from "react-icons/lia";
 import { LiaToggleOnSolid } from "react-icons/lia";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = ({ theme, setTheme }) => {
   const [active, setActive] = useState(1);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     if (theme === "dark") {
@@ -15,14 +18,31 @@ const Navbar = ({ theme, setTheme }) => {
   }, [theme]);
 
   return (
-    <nav className=" flex  w-[1090px] h-[60px] justify-between items-center my-5  mt-8">
-      <div className=" flex gap-10">
-        <img
-          className=" min-w-[117px] min-h-[35px] dark:bg-primary-white  rounded-md  p-2"
-          src={Logo}
-          alt=""
-        />
-        <div className=" flex font-bold  w-full justify-evenly gap-5 text-lg text-[20px]">
+    <nav className=" flex lg:flex-row   flex-col  lg:w-[1090px] lg:h-[60px] lg:justify-between lg:items-center my-5  mt-8 lg:px-0 px-8 w-[425px]">
+      <div className=" flex lg:flex-row flex-col  gap-10">
+        <div className=" flex  justify-center lg:justify-normal gap-20 lg:gap-0 items-center">
+          <img
+            className=" lg:min-w-[117px] lg:min-h-[35px] max-w-[160px] max-h-[65px] dark:bg-primary-white  rounded-md  p-2"
+            src={Logo}
+            alt=""
+          />
+          {!show ? (
+            <GiHamburgerMenu
+              onClick={() => setShow(true)}
+              className=" dark:text-primary-white lg:hidden a mt-1 text-2xl cursor-pointer"
+            />
+          ) : (
+            <RxCross2
+              onClick={() => setShow(false)}
+              className="a dark:text-primary-white lg:hidden mt-1 text-2xl cursor-pointer"
+            />
+          )}
+        </div>
+        <div
+          className={`  ${
+            !show ? "hidden lg:flex" : ""
+          } flex flex-col  lg:flex-row  font-bold  w-full justify-evenly gap-5 text-lg text-[20px]`}
+        >
           <button
             onClick={() => setActive(1)}
             className={` hover:cursor-pointer hover:border-b-primary-red hover:border-b-2    ${
@@ -65,7 +85,11 @@ const Navbar = ({ theme, setTheme }) => {
           </button>
         </div>
       </div>
-      <div className=" flex gap-16">
+      <div
+        className={` ${
+          !show ? "hidden lg:flex" : ""
+        } flex lg:gap-16 gap-8  lg:mx-0 mx-16  lg:flex-row flex-col items-center lg:mt-0 mt-8 lg:justify-normal justify-between`}
+      >
         <button className=" text-4xl  hover:cursor-pointer dark:text-primary-white">
           {theme === "light" && (
             <LiaToggleOffSolid
